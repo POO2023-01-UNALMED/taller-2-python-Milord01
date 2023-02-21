1d8b80f8816f855822278e9a9c6b278ab2d312bf
Milord01 <124930730+Milord01@users.noreply.github.com>
2023-02-20 19:01:30 -0500

    class py

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ class Auto:
         b=True
         if self.motor.registro!= self.registro:
             b=False
-        for i in self.__Asiento__.registro():
-            if self.asientos!=i:
+
+
+        for i in self.asientos:
+            if i!=Asiento.registro[i]:
                 b=False
+
+
         if b==True:
             return("Auto original")
         else:
